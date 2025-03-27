48c5e604c045a4864a79f7995d125f8ee855f261
KassFequet <kassaundra.fequet@keyin.com>
2025-03-27 09:20:17 -0230

    lesson 11

--- March/Lesson 11 - Exercise files/index.js
+++ March/Lesson 11 - Exercise files/index.js
@@ -27,3 +27,29 @@ const com2Format = new Intl.NumberFormat("en-CA", {
 });
 
 // Start function definitions here.
+
+function ShowStuff() {
+  // show a line that reads Hello World! in the main left div.
+
+  Msg = "<h1>Hello World!</h1>";
+  document.getElementById("ShowMsg").innerHTML = Msg;
+}
+
+function AddNums() {
+  // add two numbers and show the result
+
+  let Num1 = prompt("Enter the first number: ");
+  Num1 = parseFloat(Num1);
+
+  let Num2 = prompt("Enter the second number: ");
+  Num2 = parseFloat(Num2);
+
+  let Answer = Num1 + Num2;
+
+  Msg = "The sum of " + Num1 + " and" + Num2 + " is:<br /><h1>" + Answer + ".</h1><br />";
+  document.getElementById("ShowMsg").innerHTML = Msg;
+
+
+  // notice that is asked for the input before the page loaded
+  //
+}
